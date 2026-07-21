d0c895b8e9a92fdc049d74634158c85801fc718a
Necromensor <spooky.satyam@gmail.com>
2026-07-21 20:41:00 +0530

    update pages file

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,13 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Button from "./Button";
+import Input from "./Input";
 
 export{
     Header,
@@ -12,4 +19,11 @@ export{
     Logo,
     LogoutBtn,
     RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Button,
+    Input,
 }
